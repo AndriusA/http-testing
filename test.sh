21ac8d9339bd680aae264e1a6311793a42e37612
Andrius Aucinas <aaucinas@brave.com>
2019-11-15 10:34:06 +0000

    network presets for running tests

--- test.sh
+++ test.sh
@@ -2,6 +2,9 @@
 
 url=$1
 http=$2
+NETWORK=$3
+ID=$4
+iteration=$5
 
 sudo -v
 
@@ -11,14 +14,14 @@ chrome_flags=""
 if [[ $http == "http1" ]]; then
     chrome_flags="--disable-http2"
 elif [[ $http == "http3" ]]; then
-	chrome_flags="--quic-version=h3-24 --enable-quic"
+	chrome_flags="--quic-version=h3-23 --enable-quic"
 fi
 
 args=(
 	$url
   	--preset perf
-	--output json html
-	--output-path lighthouse-http/perf.json --save-assets
+	--output json
+	--output-path lighthouse-http/$ID-$http-$NETWORK-$iteration.perf.json --save-assets
 	--emulated-form-factor desktop
 	--throttling-method provided
 	--quiet
@@ -52,7 +55,30 @@ throttle_start () {
 # retranspre=$(sudo netstat -s -p tcp | grep "retransmitted" | awk 'NR==1{print $1}')
 
 echo "Throttling network connection..."
-throttle_start 4048Kbit/s 512Kbit/s 20ms 0.1   
+
+case $NETWORK in
+	3G )
+		throttle_start 780Kbit/s 330Kbit/s 100ms 0
+		;;
+	DSL )
+		throttle_start 2Mbit/s 256Kbit/s 5ms 0
+		;;
+	LTE )
+		throttle_start 50Mbit/s 10Mbit/s 50ms 0
+		;;
+	EDGE )
+		throttle_start 240Kbit/s 200Kbit/s 400ms 0
+		;;
+	LOSSYFAST )
+		throttle_start 50Mbit/s 10Mbit/s 50ms 0.1
+		;;
+	LOSSYSLOW )
+		throttle_start 1Mbit/s 1Mbit/s 50ms 0.1
+		;;
+	VERYLOSSY )
+		throttle_start 50Mbit/s 10Mbit/s 50ms 0.2
+		;;
+esac
 
 echo "Measuring..."
 lighthouse "${args[@]}" --chrome-flags="$chrome_flags"
